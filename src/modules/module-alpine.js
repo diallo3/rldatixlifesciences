cf06f9aa2bc4b53509871709d4d78f99f6a92c8d
Diallo Hudgins <d@diallohudgins.com>
2024-05-27 10:05:32 -0400

    Add new macros and partials
    Signed-off-by: Diallo Hudgins <d@diallohudgins.com>

--- src/modules/module-alpine.js
+++ src/modules/module-alpine.js
@@ -2,20 +2,21 @@ import Alpine from 'alpinejs';
 import persist from '@alpinejs/persist';
 import focus from '@alpinejs/focus';
 import collapse from '@alpinejs/collapse';
+import moduleAlpineNavHighlighter from '/src/modules/module-alpine-navHighlighter.js';
 
-
-
-    Alpine.plugin(
+Alpine.plugin(
         [
             persist, 
             focus, 
             collapse
         ]
-    );
+);
+   
 
 export function initializeAlpine() {
     // Alpine.js
     if(!window.Alpine) {
+        Alpine.data('visibleNavHighlighter', moduleAlpineNavHighlighter);
         window.Alpine = Alpine;
         Alpine.start();
     }
